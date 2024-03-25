1060c0b8c04463f15c0fff3c7eedba596611a9fd
Dawn36 <85417047+Dawn36@users.noreply.github.com>
2024-03-26 01:11:08 +0500

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,9 +3,9 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{ asset('theme/assets/media/logos/white-logo.png')}}" class="logo" alt="Laravel Logo">
+<img src="{{asset('theme/assets/media/logos/white-logo.png')}}" class="logo" alt="Laravel Logo">
 @else
-<img src="{{ asset('theme/assets/media/logos/white-logo.png')}}" class="logo" alt="Laravel Logo">
+<img src="{{asset('theme/assets/media/logos/white-logo.png')}}" class="logo" alt="Laravel Logo">
 @endif
 </a>
 </td>
